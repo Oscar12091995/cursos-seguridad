b98309d2baad3b1add6b951c26de08fe85568e82
Oscar12091995 <quike0116@hotmail.com>
2022-05-11 09:20:18 -0500

    Seo

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -88,11 +88,7 @@ class PaymentController extends Controller
         $result = $payment->execute($execution, $apiContext); */
         
         
-        //despues de pagar y proceder al curso a inscribirse
-        //traducciones al español
-        //entrada al curso
-        //llevar curso
-        //registro    
+        //despues de pagar y proceder al curso a inscribirse    
         
         $course->students()->attach(auth()->user()->id);
         
