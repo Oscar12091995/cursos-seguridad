a40bb43d859f402b964f678398dd378e9b1553ab
Oscar12091995 <quike0116@hotmail.com>
2022-06-16 10:21:45 -0500

    pay

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -120,7 +120,7 @@ class PaymentController extends Controller
         $execution->setPayerId($_GET['PayerID']);
         $result = $payment->execute($execution, $apiContext); */
 
-        //no funciona
+        //funiona ya
         
         $course->students()->attach(auth()->user()->id);
         
